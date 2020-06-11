f0ae740035ce88f4f83db0ae3520244ffb3cbfd1
UmemotoCtrl <>
2020-06-11 12:06:51 +0900

    * モバイルに対応 * markdown練習のリンクを修正

--- js/markdownjs.js
+++ js/markdownjs.js
@@ -36,10 +36,14 @@ var loadMd = function ( argText, gaSend ) {
 		return response.text();
 	}).then(function(data) {
 		article.innerHTML = mdp.render(translateInnerAnchor(data));
-		var h1 = document.querySelector("div#article>h1:first-child");
-		document.title = (h1.innerHTML||"");
+		// var h1 = document.querySelector("div#article>h1:first-child")|[];
+		if (/^# (.+?)$/m.test(data))
+			document.title = data.match(/^# (.+?)$/m)[1];
+		titleH1.innerHTML = document.title;
 		// For Google Analytics SPA ---
 		if ( gaSend ) {
+			HBIconState = true;
+			HBIcon.onclick();
 			window.history.pushState(null, null, argText);
 			scrollTo(0, 0);  // absolute (hr, ver)
 			ga('set', 'page', "/"+location.search);
@@ -54,19 +58,53 @@ var loadMd = function ( argText, gaSend ) {
 var mdp;
 var article;
 var topMenu;
+var Hamburger;
+var HBMenu;
+var HBIcon;
+var HBIconState = false;
+var titleH1;
+var NavBar;
 
 window.onload = function() {
 	mdp = makeMDP();
 	article = document.getElementById("article");
 	topMenu = document.getElementById("TopMenu");
-	mdInput = document.getElementById("mdInput");
-	raw     = document.getElementById("raw");
+	titleH1 = document.getElementById("Title");
+	Hamburger = document.getElementById("Hamburger");
+	HBMenu = document.getElementById("HBMenu");
+	HBIcon = document.getElementById("HBIcon");
+	NavBar = document.getElementById("NavBar");
+
+	if ( navigator.userAgent.match(/iPhone|Android.+Mobile/) ) {
+	// if (true) {
+		topMenu.style.display = 'none';
+		NavBar.style.maxHeight = '5rem';
+		titleH1.style.fontSize = '3rem';
+		const root = document.documentElement;
+		root.style.setProperty('--nav-heigh', '5rem');
+	} else {
+		Hamburger.style.display = 'none';
+	}
+		
 	// Load top menu
 	fetch('./md/topMenu.md').then(function(response) {
 		return response.text();
 	}).then(function(data) {
 		topMenu.innerHTML = mdp.render(translateInnerAnchor(data));
+		HBMenu.innerHTML = mdp.render(translateInnerAnchor(data));
 	});
+	this.HBIcon.onclick = function(){
+		if (HBIconState) {
+			HBIconState = false;
+			HBIcon.style.backgroundImage = 'url(/img/hamburger.svg)';
+			HBMenu.style.display = 'none';
+		} else {
+			HBIconState = true;
+			HBIcon.style.backgroundImage = 'url(/img/cross.svg)';
+			HBMenu.style.display = 'block';
+		}
+	};
+
 	// Load Main md file
 	loadMd( "./"+location.search, false);
 }
